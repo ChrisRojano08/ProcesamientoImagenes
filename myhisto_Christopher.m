255808e5a1db209021e3b95b527693262092661b
ChrisRojano08 <crisrojano8@gmail.com>
2022-02-21 16:55:13 -0600

    New images

--- myhisto_Christopher.m
+++ myhisto_Christopher.m
@@ -6,22 +6,40 @@ img1 = imread('img.png');
 
 [fil, col, k1] = size(img1);
 
-histo = zeros([1 257]);
-for k=1:k1
+R = img1(:,:,1);
+G = img1(:,:,2);
+B = img1(:,:,3);
+
+histo = zeros(1,256);
+%for k=1:k1
     for i=1:fil
         for j=1:col
-                n = img1(i,j,k)+1;
+                n = R(i,j)+1;
                 histo(n) =  histo(n)+1;
         end
     end
-end
+%end
+%Generar histograma
+HR = imhist(R);
+HG = imhist(G);
+HB = imhist(B);
 
-for i=1:257
-    fprintf('%g: %g\n',i-1,histo(i))
-end
+%Mostrar imagenes
+subplot(2,2,1);
+imshow(img1);
+title("Imagen de entrada");
+subplot(2,2,2);
+imshow(G);
+title("Green");
+subplot(2,2,3);
+plot(histo);
+title("Histograma");
 
+% for i=1:257
+%     fprintf('%g: %g\n',i-1,histo(i))
+% end
 %A = 0:256;
-%hist(histo,A)
+% hist(histo)
 
 
 
